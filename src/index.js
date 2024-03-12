75cf3faacd2a824deb69516c60afc8cc0038b633
NdivhuwoR <146739315+NdivhuwoR@users.noreply.github.com>
2024-03-12 15:44:45 +0200

    Added forecast API

--- src/index.js
+++ src/index.js
@@ -15,8 +15,12 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"
+   class="weather-app-icon" />`;
+
+   getForecast(response.data.city);
 }
+
 
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -52,7 +56,15 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
+function getForecast(city) {
+  let apiKey = "6et32f71afb5cb26a0c8d90ea79o94fb";
+  let apiUrl = 'https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric';
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response){
+  console.log(response.data);
+
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
